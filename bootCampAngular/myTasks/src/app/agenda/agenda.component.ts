f8ded06d081e9d7ce1cb6d989afb2c7974dc3f87
unknown <meloferreiragabriel17@gmail.com>
2021-04-16 08:15:28 -0300

    commite aula 16/04

---
+++ bootCampAngular/myTasks/src/app/agenda/agenda.component.ts
@@ -0,0 +1,18 @@
+/* eslint-disable prettier/prettier */
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-agenda',
+  templateUrl: './agenda.component.html',
+  styleUrls: ['./agenda.component.scss']
+})
+export class AgendaComponent implements OnInit {
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  constructor() { };
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  ngOnInit(): void {
+  }
+
+}
--- bootCampAngular/myTasks/src/app/app-routing.module.ts
+++ bootCampAngular/myTasks/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 /* eslint-disable prettier/prettier */
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AgendaComponent } from './agenda/agenda.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'agenda',
+    component: AgendaComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
